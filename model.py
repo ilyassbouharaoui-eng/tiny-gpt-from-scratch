d5f05dfd23ff4a533925e59bd6b2dac1f37cef2d
ilyassbouharaoui-eng <ilyassbouharaoui@gmail.com>
2026-08-11 01:34:35 +0200

    Step 52: generate_sequence

--- model.py
+++ model.py
@@ -374,8 +374,18 @@ def sample_next_token(p_matrix, current_id, rng):
     pr = p_matrix[current_id]
     return int(rng.choice(range(len(p_matrix[0])) , p = pr))
 
-# Step 52 - generate_sequence (not yet solved)
-# TODO: implement
+# Step 52 - generate_sequence
+def generate_sequence(p_matrix, start_id, length, rng):
+    """Autoregressively sample `length` token ids from a bigram matrix, starting with `start_id`."""
+    l = [start_id]
+    i = 0
+    current_id = start_id
+    while i < length-1:
+        x = sample_next_token(p_matrix,current_id,rng)
+        l.append(x)
+        current_id = x
+        i+=1
+    return np.array(l)
 
 # Step 53 - decode_generated_sequence (not yet solved)
 # TODO: implement
