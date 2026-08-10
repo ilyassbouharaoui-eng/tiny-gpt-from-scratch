685c041490f7f1ccca0d68c127f50a319c49300d
ilyassbouharaoui-eng <ilyassbouharaoui@gmail.com>
2026-08-10 13:55:48 +0200

    Step 43: stack_y_batch

--- model.py
+++ model.py
@@ -307,8 +307,15 @@ def stack_x_batch(data, offsets, block_size):
         res.append(slice_x_at_offset(data,i,block_size))
     return np.array(res)
 
-# Step 43 - stack_y_batch (not yet solved)
-# TODO: implement
+# Step 43 - stack_y_batch
+import numpy as np
+
+def stack_y_batch(data, offsets, block_size):
+    """Stack per-offset Y windows into a 2D (B, block_size) target matrix."""
+    res = []
+    for i in offsets:
+        res.append(slice_y_at_offset(data,i,block_size))
+    return np.array(res)
 
 # Step 44 - get_batch (not yet solved)
 # TODO: implement
