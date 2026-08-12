fcfa0b1e583fcf6288a8a05560fe20250be9b738
ilyassbouharaoui-eng <ilyassbouharaoui@gmail.com>
2026-08-12 13:14:30 +0200

    Step 60: forward_logits_onehot

--- model.py
+++ model.py
@@ -432,8 +432,9 @@ def one_hot_encode_batch(ids, vocab_size):
         M[i][ids[i]] = 1
     return M
 
-# Step 60 - forward_logits_onehot (not yet solved)
-# TODO: implement
+# Step 60 - forward_logits_onehot
+def forward_logits_onehot(onehot, w_matrix):
+    return matmul(onehot,w_matrix)
 
 # Step 61 - observe_lookup_equivalence (not yet solved)
 # TODO: implement
